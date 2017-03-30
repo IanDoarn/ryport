3c8fc9f4fbd0ba93eceac4ae2ff9cd7456422b32
iandoarn <ian.doarrn@zimmerbiomet.com>
2017-03-30 12:27:18 -0500

    Added xml building methods
    Signed-off-by: iandoarn <ian.doarrn@zimmerbiomet.com>

--- xmller/generate.py
+++ xmller/generate.py
@@ -1,15 +1,53 @@
 from lxml import etree
 
-# create XML
-root = etree.Element('root')
-root.append(etree.Element('child'))
-# another child with text
-child = etree.Element('child')
-child.text = 'some text'
-root.append(child)
 
-# pretty string
-s = etree.tostring(root, pretty_print=True)
+class xmller:
 
-with open('test.xml', 'w')as f:
-    f.write(s.decode("utf-8"))
+    def __init__(self, root='root'):
+        self.root = etree.Element(root)
+
+    @staticmethod
+    def write_file(root, file_name):
+        xml_object = etree.tostring(root, pretty_print=True)
+        with open(file_name, 'w')as xml_file:
+            xml_file.write(xml_object.decode('utf-8'))
+        xml_file.close()
+
+    @staticmethod
+    def create_root(name):
+        return etree.Element(name)
+
+    @staticmethod
+    def create_child(name, text):
+        child = etree.Element(name)
+        child.text = text
+        return child
+
+    @staticmethod
+    def add_child(root, child):
+        root.append(child)
+
+
+    def create_xml_data(self, file_name, query, server_information=[{}], information=[{}], use_default_root=True, custom_root=None):
+
+        if use_default_root is False:
+            root = self.create_root(custom_root)
+        else:
+            root = self.root
+
+        children = []
+
+        children.append(self.create_child('query', query))
+
+        for object in server_information:
+            for key, value in object.items():
+                children.append(self.create_child(str(key), str(value)))
+
+        for object in information:
+            for key, value in object.items():
+                children.append(self.create_child(str(key), str(value)))
+
+        for child in children:
+            self.add_child(root, child)
+
+        self.write_file(self.root, file_name)
