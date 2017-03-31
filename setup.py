1a8ea3e1bf8f383d014e6d9f9fd3dc173e1f78e3
IanDoarn <ian.doarn@zimmerbiomet.com>
2017-03-31 16:30:29 -0500

    Actually created setup.py
    Signed-off-by: IanDoarn <ian.doarn@zimmerbiomet.com>

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='ryport',
+    version='1.0.0',
+    packages=['ryport', 'ryport.pgsql', 'ryport.xml_builder'],
+    url='https://github.com/IanDoarn/ryport',
+    license='MIT',
+    author='Ian Doarn',
+    author_email='ian.doarn@zimmerbiomet.com',
+    description='Automated reporting tool for postgres'
+)
