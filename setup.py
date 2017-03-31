638f938764d6278cdfc346a855243ed47936a759
IanDoarn <ian.doarn@zimmerbiomet.com>
2017-03-31 16:40:37 -0500

    updates to setup.py to include package dependencies
    Signed-off-by: IanDoarn <ian.doarn@zimmerbiomet.com>

--- setup.py
+++ setup.py
@@ -1,8 +1,12 @@
-from distutils.core import setup
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
 
 setup(
     name='ryport',
-    version='1.0.1',
+    version='1.0.2',
+    install_requires=['psycopg2', 'lxml', 'XlsxWriter'],
     packages=['ryport', 'ryport.pgsql', 'ryport.xml_builder'],
     url='https://github.com/IanDoarn/ryport',
     license='MIT',
