a239899e34ac3a148d2b4aa8bed90682655a255e
Ian Doarn <ian.doarn@zimmerbiomet.com>
2017-06-13 11:30:07 -0500

    Updates to setup.py removing self module check
    Signed-off-by: IanDoarn <Ian.Doarn@zimmerbiomet.com>

--- setup.py
+++ setup.py
@@ -48,64 +48,10 @@ short_desc = "Softwrapper and reporting tool for Postgres"
 # the user has them first, then adding them
 # to the list of required if they don't.
 
-install_requires = []
-modules_to_update = []
-# Pull required modules from requirements.txt
-with open(os.path.join(file_path, 'requirements.txt'), 'r')as req_file:
-    req_modules = req_file.readlines()
-req_file.close()
-
-# Attempt to import each module
-for mod in req_modules:
-    _mod = mod.replace('\n', '').split('>=')
-    try:
-        print('Attempting to import {}'.format(_mod[0]))
-        # Try to import
-        if _mod[0] == 'mysql-connector-python':
-            module = importlib.import_module('mysql.connector')
-        else:
-            module = importlib.import_module(_mod[0])
-        # Check version of module
-        mod_dist = get_distribution(_mod[0])
-
-        if len(set(list(mod_dist.version)).intersection(list(ascii_letters))) > 0:
-            # module version has letters in it, attempt to upgrade it
-            print('Could not verify version of {}'.format(_mod[0]))
-            modules_to_update.append(_mod[0])
-        else:
-            mod_version = int(''.join(mod_dist.version.split('.')))
-            req_mod_version = int(''.join(_mod[1].split('.')))
-            if  mod_version < req_mod_version:
-                print("{} is installed but is not the required version:"
-                      "Installed [{}] Required [{}]".format(_mod[0],
-                                                            str(mod_version),
-                                                            str(req_mod_version)))
-                modules_to_update.append(_mod[0])
-            else:
-                print("{} is installed and is the correct version [>={}]".format(_mod[0],
-                                                                               _mod[1]))
-        # delete from memory if successful
-        del module
-    except ModuleNotFoundError as error:
-        # could not import so add to install requires
-        print('Could not import {}'.format(_mod[0]))
-        install_requires.append(mod.replace('\n', ''))
-
-
-# Attempt to update modules
-def update_modules():
-    if len(modules_to_update) != 0:
-        for mod in modules_to_update:
-            try:
-                subprocess.call(['pip',
-                                 'install',
-                                 mod,
-                                 '--upgrade'], shell=True)
-            except Exception as error:
-                print('Unable to update {} to the required / latest version. '
-                      'Please resolve this issue to ensure stability. '
-                      'Error: [{}]'.format(mod, str(error)))
-
+install_requires = ['mysql-connector-python',
+                    'psycopg2',
+                    'lxml',
+                    'XlsxWriter']
 
 # Create package list of packages in ryport
 excluded = ['.git', '.idea', '__pycache__']
